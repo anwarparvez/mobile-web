d402cb5f38e818d0e1d1fd60f92a1fe697b40fcf
Anwar Parvez <parvez.uz@gmail.com>
2013-05-25 00:14:40 +0600

    optimiztion

--- export/halifax.php
+++ export/halifax.php
@@ -5,17 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-        <!--Local-->
-        <!--<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
-        <script src="jquerymobile/jquery-1.8.2.min.js"></script>
-        <script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
-        -->
-        <!-- <link rel="stylesheet"  href="css/themes/default/jquery.mobile-1.2.1.css" />
-         <link rel="stylesheet" href="_assets/css/jqm-docs.css"/>
-         <script src="js/jquery.js"></script>
-         <script src="docs/_assets/js/jqm-docs.js"></script>
-         <script src="js/jquery.mobile-1.2.1.js"></script>-->
-
         <!--jquery mobile -->
         <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.css" />
         <script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
@@ -26,7 +15,6 @@
         <script type="text/javascript" src="http://dev.jtsage.com/cdn/datebox/latest/jqm-datebox.core.min.js"></script>
         <script type="text/javascript" src="http://dev.jtsage.com/cdn/datebox/latest/jqm-datebox.mode.calbox.min.js"></script>
         <script type="text/javascript" src="http://dev.jtsage.com/cdn/datebox/i18n/jquery.mobile.datebox.i18n.en_US.utf8.js"></script>
-
 
         <!-- others -->
         <link rel="stylesheet" href="include/stylesheet.css" />
--- sample_request.sql
+++ sample_request.sql
@@ -1,11 +1,9 @@
  CREATE TABLE sample_request (
-`_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`user_id` INT NOT NULL ,
-`product` VARCHAR( 20 ) NOT NULL ,
-`colour` VARCHAR( 20 ) NOT NULL ,
-`finish` VARCHAR( 20 ) NOT NULL
+_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+project_id INT NOT NULL ,
+product VARCHAR( 20 ) NOT NULL ,
+colour VARCHAR( 20 ) NOT NULL ,
+finish VARCHAR( 20 ) NOT NULL
 ) ENGINE = MYISAM
 
-
- ALTER TABLE `sample_request` CHANGE `user_id` `product_id` INT( 11 ) NOT NULL
- ALTER TABLE `sample_request` CHANGE `product_id` `project_id` INT( 11 ) NOT NULL 
+ ALTER TABLE sample_request CHANGE user_id project_id INT( 11 ) NOT NULL
